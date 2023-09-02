812556585c6c5a560d7dae7523eab5f147f5fd03
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-09-02 05:05:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,10 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const pool = require("./db");
+const http = require("http");
 
-//middlewarte
-app.use(cors());
-app.use(express.json());
-
-// making a post
-app.post("/users", async (req, res) => {
-    try {
-        const { name, location, pay } = req.body;
-        const newData = await pool.query(
-            "INSERT INTO users(id,name,location,pay) VALUES(nextval('users_id_seq'),$1,$2,$3) RETURNING *",
-            [name, location, pay]
-        )
-        res.json(newData.rows)
-
-    } catch (error) {
-        console.log(error.message)
-    }
-})
-
-app.delete("/users", async (req, res) => {
-
-    try {
-        const data = await pool.query(
-            "DELETE FROM users;"
-        );
-        res.json(data)
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-app.get("/users", async (req, res) => {
-    try {
-        const data = await pool.query(
-            "SELECT * FROM users;"
-        );
-        res.json(data)
-    } catch (error) {
-        console.log(error.message)
-    }
-})
-
-app.post("/gassers", async (req, res) => {
-    try {
-        const { name, location, bank } = req.body;
-        const newData = await pool.query(
-            "INSERT INTO gassers(id,name,location,bank) VALUES(nextval('gassers_id_seq'),$1,$2,$3) RETURNING *",
-            [name, location, bank]
-        )
-        res.json(newData.rows)
-
-    } catch (error) {
-        console.log(error.message)
-    }
-
-});
-app.delete("/gassers", async (req, res) => {
-
-    try {
-        const data = await pool.query(
-            "DELETE FROM gassers;"
-        );
-        res.json(data)
-    } catch (error) {
-        console.log(error)
-    }
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
 });
 
-app.get("/gassers", async (req, res) => {
+const PORT = process.env.PORT || 3000;
 
-    try {
-        const data = await pool.query(
-            "SELECT * FROM gassers;"
-        )
-        res.json(data);
-    } catch (error) {
-        console.log(error.message)
-    }
-})
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-    console.log("server works")
-})
+server.listen(PORT, () => console.log("server is running"));
