67fd755a31bed29f0d32771cbbcb39009ee8b3d5
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-09-03 01:15:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,4 +26,4 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log("server is running"));
+app.listen(PORT, () => console.log("server is running"));
