4919fba4ee28901dfb1e89ff5d4baf44cab2da36
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-09-03 02:26:39 -0400

    Add files via upload

--- browser.js
+++ browser.js
@@ -5,18 +5,14 @@ const body = {
 };
 
 fetch("https://codevolution-demo-8o2k.onrender.com/hello", {
-    headers: { "Content-Type": "application/json" }, // Corrected Content-Type
+    headers: { "Content-Type": "application/json" },
     method: "POST",
     body: JSON.stringify(body)
-}).then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json(); // You can use .json() to parse JSON response
-}).then(data => {
-    // Handle the data here, for example, log it
-    console.log(data);
-}).catch(error => {
-    // Handle errors here
-    console.error('There was a problem with the fetch operation:', error);
-});
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
