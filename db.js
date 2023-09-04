5e8c1766c4f0f9d7707d8de1d1114557868deaec
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-09-03 22:32:36 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const Pool = require("pg").Pool;
 
 const POOL = new Pool({
-    connectionString: "postgres://monk:Le4h5PJTF7wRmdV56nNI0g5RvKg8FrLT@dpg-cjq6aje1208c73824su0-a.oregon-postgres.render.com/try_kmt8"
+    connectionString: "postgres://monk:Le4h5PJTF7wRmdV56nNI0g5RvKg8FrLT@dpg-cjq6aje1208c73824su0-a.oregon-postgres.render.com/try_kmt8",
 
+    ssl: {
+        rejectUnauthorized: false, // You may need to adjust this based on your PostgreSQL setup
+    },
     // user: "playabook",
     // password: "8896",
     // database: "try",
