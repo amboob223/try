28e24fea10e980e52f7d712b4e65d43f40419e3c
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-09-03 20:55:51 -0400

    Add files via upload

--- browser.js
+++ browser.js
@@ -1,17 +1,27 @@
 const main = document.getElementById("main");
+const btn = document.getElementById("btn");
 
-const body = {
-    main: main.value
-};
 
-fetch("https://codevolution-demo-8o2k.onrender.com/hello", {
-    headers: { "Content-Type": "application/json" },
-    method: "POST",
-    body: JSON.stringify(body)
+
+btn.addEventListener("click", async (event) => {
+    event.preventDefault()
+    try {
+        const body = {
+            main: main.value
+        };
+
+        fetch("https://codevolution-demo-8o2k.onrender.com/hello", {
+            headers: { "Content-Type": "application/json" },
+            method: "POST",
+            body: JSON.stringify(body)
+        }).then(response => response.json())
+            .then(data => {
+                console.log(data);
+            }).catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+
+    } catch (error) {
+        console.log(error)
+    }
 })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    }).catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
